c4bf21489d3485514e039f94cbcdb94a41e0bae3
MartinelliNicolo <nicolomartinelli1401@gmail.com>
2025-10-13 11:20:19 +0200

    aggiunta search

--- script.js
+++ script.js
@@ -2,10 +2,15 @@
   const closeModalBtn = document.getElementById('closeModal');
   const modal = document.getElementById('modal');
   const btnSend = document.getElementById('send');
-  const btnSearch = document.getElementById('tooltipSearch');
+  const openSearch = document.getElementById('tooltipSearch');
+  const btnSearch = document.getElementById('btnSearch');
+  const closeSearch = document.getElementById('closeSearch');
   const search = document.getElementById('search');
   const form = document.getElementById('form');
+  const formSearch = document.getElementById('formSearch');
+  const btnHome = document.getElementById('home');
   let i = 0;
+  let id = 1;
   let issues = JSON.parse(localStorage.getItem('issues')) || [];
 
     openModalBtn.addEventListener('click', () => {
@@ -25,11 +30,32 @@
       }
     });
 
-    btnSearch.addEventListener('click', () => {
+    openSearch.addEventListener('click', () => {
       search.classList.remove('hidden');
     });
 
+    closeSearch.addEventListener('click', () => {
+      search.classList.add('hidden');
+      formSearch.reset();
+    });
+
+    // Chiudi ricerca cliccando fuori
+    window.addEventListener('click', (e) => {
+      if(e.target === search) {
+        search.classList.add('hidden');
+      }
+    });
+
+    btnHome.addEventListener('click', () => {
+      modal.classList.add('hidden');
+      form.reset();
+      document.getElementById('desc').value = '';
+      search.classList.add('hidden');
+      formSearch.reset();
+    });
+
     uploadCard();
+    insertcard(issues);
 
     //issues = [];
     //localStorage.setItem('issues', null);
@@ -47,7 +73,9 @@
         return;
       }
 
-      issues.push({ name, title, priority, description });
+      issues.push({ name, title, priority, description, id });
+
+      insertcard(issues);
 
       saveInLocalStorage();
       updateCard(name, title, priority, description);
@@ -57,17 +85,43 @@
       modal.classList.add('hidden');
     });
 
+    btnSearch.addEventListener('click', () => {
+      const name = document.getElementById('nameSearch').value;
+      const title = document.getElementById('titleSearch').value;
+
+      if(!name && !title) {
+        alert('Per favore, compila tutti i campi.');
+        return;
+      }
+
+      find();
+
+      formSearch.reset();
+      search.classList.add('hidden');
+    });
+
     function saveInLocalStorage() {
       localStorage.setItem('issues', JSON.stringify(issues));
     }
 
     function uploadCard() {
       for (i = 0; i < issues.length; i++)
-        document.getElementById("card1").innerHTML += issues[i].title + "<br>" + issues[i].priority + "<br>" + issues[i].description + "<br>" + issues[i].name;
-      console.log(card1);
+        document.getElementById("card1").innerHTML += issues[i].title + "<br>" + issues[i].priority + "<br>" + issues[i].description + "<br>" + issues[i].name + "<br>";
     }
 
     function updateCard(name, title, priority, description) {
-      document.getElementById("card1").innerHTML += title + "<br>" + priority + "<br>" + description + "<br>" + name;
-      console.log(card1);
+      document.getElementById("card1").innerHTML += title + "<br>" + priority + "<br>" + description + "<br>" + name + "<br>";
+    }
+
+    function find() {
+      let found = false;
+      for (i = 0; i < issues.length; i++) {
+        if (issues[i].name == document.getElementById('nameSearch').value || issues[i].title == document.getElementById('titleSearch').value) {
+          document.getElementById("card2").innerHTML += issues[i].title + "<br>" + issues[i].priority + "<br>" + issues[i].description + "<br>" + issues[i].name + "<br>";
+          found = true;
+        }
+      }
+      if (!found) {
+        alert('Nessuna issue trovata con questo nome e titolo.');
+      }
     }
